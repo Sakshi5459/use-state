89c8a7e4effe31465b62b417369f280c6e4f1ee7
Sakshi5459 <sakshi.mahendrakar@exhibytesolution.com>
2023-08-11 16:29:47 +0530

    hello git

--- src/App.js
+++ src/App.js
@@ -126,81 +126,96 @@
 // ];
 
 //6
-import { useReducer } from "react";
+// import { useReducer } from "react";
+// import AddTask from "./AddTask";
+// import TaskList from "./TaskList";
+
+// export default function TaskApp() {
+//   const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+//   function handleAddTask(text) {
+//     dispatch({
+//       type: "added",
+//       id: nextId++,
+//       text: text,
+//     });
+//   }
+
+//   function handleChangeTask(task) {
+//     dispatch({
+//       type: "changed",
+//       task: task,
+//     });
+//   }
+
+//   function handleDeleteTask(taskId) {
+//     dispatch({
+//       tyoe: "deleted",
+//       id: taskId,
+//     });
+//   }
+
+//   return (
+//     <>
+//       <h1>Prague itinerary</h1>
+//       <AddTask onAddTask={handleAddTask} />
+//       <TaskList
+//         tasks={tasks}
+//         onChangeTask={handleChangeTask}
+//         onDeleteTask={handleDeleteTask}
+//       />
+//     </>
+//   );
+// }
+
+// function tasksReducer(tasks, action) {
+//   switch (action.type) {
+//     case "added": {
+//       return [
+//         ...tasks,
+//         {
+//           id: action.id,
+//           text: action.text,
+//           done: false,
+//         },
+//       ];
+//     }
+//     case "changed": {
+//       return tasks.map((t) => {
+//         if (t.id === action.task.id) {
+//           return action.task;
+//         } else {
+//           return t;
+//         }
+//       });
+//     }
+//     case "deleted": {
+//       return tasks.filter((t) => t.id !== action.id);
+//     }
+//     default: {
+//       throw Error("Unknown action:" + action.type);
+//     }
+//   }
+// }
+
+// let nextId = 3;
+// const initialTasks = [
+//   { id: 0, text: "Abhi", done: true },
+//   { id: 1, text: "Shiva", done: false },
+//   { id: 2, text: "Manisha", done: false },
+// ];
+
+//7
 import AddTask from "./AddTask";
 import TaskList from "./TaskList";
+import { TasksProvider } from "./TaskContext";
 
 export default function TaskApp() {
-  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
-
-  function handleAddTask(text) {
-    dispatch({
-      type: "added",
-      id: nextId++,
-      text: text,
-    });
-  }
-
-  function handleChangeTask(task) {
-    dispatch({
-      type: "changed",
-      task: task,
-    });
-  }
-
-  function handleDeleteTask(taskId) {
-    dispatch({
-      tyoe: "deleted",
-      id: taskId,
-    });
-  }
-
   return (
-    <>
-      <h1>Prague itinerary</h1>
-      <AddTask onAddTask={handleAddTask} />
-      <TaskList
-        tasks={tasks}
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
-      />
-    </>
+    <TasksProvider>
+      <h1>Day off in tokyo</h1>
+      <AddTask />
+      <TaskList />
+    </TasksProvider>
   );
 }
-
-function tasksReducer(tasks, action) {
-  switch (action.type) {
-    case "added": {
-      return [
-        ...tasks,
-        {
-          id: action.id,
-          text: action.text,
-          done: false,
-        },
-      ];
-    }
-    case "changed": {
-      return tasks.map((t) => {
-        if (t.id === action.task.id) {
-          return action.task;
-        } else {
-          return t;
-        }
-      });
-    }
-    case "deleted": {
-      return tasks.filter((t) => t.id !== action.id);
-    }
-    default: {
-      throw Error("Unknown action:" + action.type);
-    }
-  }
-}
-
-let nextId = 3;
-const initialTasks = [
-  { id: 0, text: "Abhi", done: true },
-  { id: 1, text: "Shiva", done: false },
-  { id: 2, text: "Manisha", done: false },
-];
